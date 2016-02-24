debbdc3e50c4503780a451825f40aa269d5aaf6a
Mai Van Hien <hienmv94@gmail.com>
2016-02-24 15:15:26 +0700

    download file

--- app/controllers/bitbuckets_controller.rb
+++ app/controllers/bitbuckets_controller.rb
@@ -1,9 +1,18 @@
 class BitbucketsController < ApplicationController
+  
   def index
-    @bitbucket = BitBucket.new login:'uet-oop', password:'oop-cnpm'
+    @repos = set_account.repos
   end
+  
   def show
   end
+  
   def create 
   end
+
+  private
+  def set_account
+  	@bitbucket = BitBucket.new(login:'uet-oop', password:'oop-cnpm')
+  end
+
 end
